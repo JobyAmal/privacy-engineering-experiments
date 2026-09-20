d0056438a3a33428f2cd91e088e2fd573186e954
Amal Joby <91418876+JobyAmal@users.noreply.github.com>
2026-09-20 12:43:27 +0530

    Add files via upload

---
+++ PII Scanner/pii_scanner.py
@@ -0,0 +1,24 @@
+import re
+
+email_pattern = r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b"
+ssn_pattern = r'\b(?!000|666|9\d{2})\d{3}[-\s]?(?!00)\d{2}[-\s]?(?!0000)\d{4}'
+
+def masked_ssn (ssn_string):
+    return f"###-##-{ssn_string[-4:]}"
+
+def masked_email (email_string):
+    return f"{email_string[:4]}*********{email_string[-8:]}"
+
+with open("database_dump.txt", "r") as file:
+    for line in file:
+        emails = re.findall(email_pattern, line)
+        ssns = re.findall(ssn_pattern, line)
+
+        if emails:
+                for email in emails:
+                    masked = masked_email(email)
+                    print("PII Alert - Email found:", masked)
+        if ssns:
+                for ssn in ssns:
+                    masked = masked_ssn(ssn)
+                    print ("PII Alert - SSN Found:", masked)
